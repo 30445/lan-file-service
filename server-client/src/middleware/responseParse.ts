cbc521de9268b8cdb1a5702c4f6338b10ba69c94
骆钰松 <304454912@qq.com>
2024-11-12 10:00:40 +0800

    优化数响应包装方法

--- server-client/src/middleware/responseParse.ts
+++ server-client/src/middleware/responseParse.ts
@@ -15,13 +15,16 @@ export default function () {
     const originalJsonFn = res.json.bind(res) // 保存原始的 json 方法
 
     res.send = function (body: any) {
-      let bodyType = typeof body
+      const bodyType = typeof body
+      const isNotPrimitiveType = !["object", "boolean", "number"].includes(bodyType)
+      // 字符串是否包含___end___，包含就不用再包装
+      const isEndString = bodyType === "string" && body.includes("___end___")
       // 请求成功，数据不是object、boolean、number、包含___end___字符串的，去json包装一下
-      if (res.statusCode >= 200 && res.statusCode < 400 && !["object", "boolean", "number"].includes(bodyType) && !(bodyType === "string" && body.includes("___end___"))) {
+      if (res.statusCode >= 200 && res.statusCode < 400 && isNotPrimitiveType && !isEndString) {
         return originalJsonFn(body)
       }
       // 兼容自定义的json格式
-      if (bodyType === "string" && body.includes("___end___")) {
+      if (isEndString) {
         let str = JSON.parse(body)
         delete str.___end___
         body = JSON.stringify(str)
